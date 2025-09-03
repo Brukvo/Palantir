06cd66fa88e9d201fc6bedfb8dface82b091afdd
Brukvo <smullerin@mail.ru>
2025-09-03 22:16:54 +0900

    изменён способ вывода преподавателей на странице отделения, исправлены ссылки в главном меню, добавлены тестовые данные

--- student_statuses.sql
+++ student_statuses.sql
@@ -1,3 +1,1 @@
--- database: /home/anton/Документы/Проекты/metodist/instance/music_school.db
-
 INSERT INTO student_statuses(status) VALUES ('учится'), ('выпущен(а)'), ('в академическом отпуске'), ('отчислен(а)');
